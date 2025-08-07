1dd1ac565766d7b1e5984a8a10dbb2d24e61b22e
Artin Karimi <artinkarimy1385@gmail.com>
2025-08-07 04:13:16 +0330

    Changing the colors and improving the Document Object Model (DOM) to make something look more professional.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
+let btnShop = document.getElementById("btnShop");
 
 btn1.addEventListener("click", () => {
     btn1.className = "btn1_Arz";
@@ -17,6 +18,10 @@ btn2.addEventListener("click", () => {
     btn1.style.fontWeight = "normal"
     btn2.className = "btn1";
     btn3.className = "btn2";
+
+    btnShop.textContent = "Sell currency";
+    btnShop.style.backgroundColor = "red";
+    btnShop.style.color = "white";
 });
 
 
@@ -25,4 +30,9 @@ btn3.addEventListener("click", () => {
     btn1.style.fontWeight = "normal"
     btn2.className = "btn2";
     btn3.className = "btn1";
+
+    btnShop.textContent = "Buy currency";
+    btnShop.style.backgroundColor = "rgb(162, 255, 13)";
+    btnShop.style.color = "black";
+
 });
